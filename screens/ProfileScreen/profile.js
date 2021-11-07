9db4fb7c44faa856a5f6398b43d46201312d1766
Mouhamad47 <74287155+Mouhamad47@users.noreply.github.com>
2021-11-08 00:47:11 +0200

    Edit Profile Page

--- screens/ProfileScreen/profile.js
+++ screens/ProfileScreen/profile.js
@@ -8,74 +8,91 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 
 
-export default function Profile({navigation}) {
-    const [userData, setUserData] = useState({});
+export default function Profile({ navigation }) {
+    const [userData, setUserData] = useState(null);
     const [greeting, setGreeting] = useState('');
 
-    const getUserProfile = async () =>{
-       api.getUserInfo().then((response)=>{
-           setUserData(response.data);
-            console.log(response.data);
+    const getUserProfile = async () => {
+        api.getUserInfo().then((response) => {
+            setUserData(response.data[0]);
+            // console.log(response.data[0].major.name);
         })
     }
-    const greetingMessage = ()=>{
+    const greetingMessage = () => {
         let d = new Date();
         let t = d.getHours();
-        if(t<12){
+        if (t < 12) {
             setGreeting("Good Morning");
         }
-        else if (t<18){
+        else if (t < 18) {
             setGreeting("Good Afternoon");
         }
-        else{
+        else {
             setGreeting("Good Evening");
         }
     }
-    const handleLogout = ()=> {
-        api.logout().then((response)=> {console.log('logout')})
+    const handleLogout = () => {
+        api.logout().then((response) => { console.log('logout') })
         AsyncStorage.clear();
         navigation.replace('Auth');
     }
 
 
-    
+
 
     useEffect(() => {
         getUserProfile();
         greetingMessage();
+        console.log(userData);
     }, [])
 
     return (
         <View style={{ backgroundColor: 'white' }}>
-             <Header/>
+            <Header />
             <View style={styles.title}>
                 <Text style={styles.titleText}>Profile</Text>
             </View>
+            {userData &&
             <View style={styles.profileView}>
+                
                 <View style={styles.imageView}>
+                    
                     <Image style={{ width: 100, height: 100 }} source={require('../../pictures/avatar.svg')} />
-                    <Text style={styles.userName}>{userData.first_name} {userData.last_name}</Text>
+                    <Text style={styles.userName}>{userData['first_name']} {userData['last_name']}</Text>
                     <Text style={styles.greeting}> {greeting} </Text>
                 </View>
-                <View>
-                    <TouchableOpacity onPress ={handleLogout}>
-                    <View style = {styles.menuItem}>
-                        <Icon name = 'sign-out' color = "#ef3737" size={25} />
-                        <Text style={styles.menuItemText}>Logout</Text>
+                <View style={styles.info}>
+                    <View style={styles.majorsection}>
+                        <Icon  name="user-md" color="#3BA5A5" size={25} />
+                        
+                            <Text style={styles.majorText}> Major: {userData.major.name}</Text>
+                        
+                        
                     </View>
+                    <View style={[styles.majorsection,{marginTop:25}]}>
+                        <Icon  name="stethoscope" color="#3BA5A5" size={25} />
+                        <Text style={styles.majorText}> Consultations:</Text>
+                    </View>
+
+                </View>
+                <View>
+                    <TouchableOpacity onPress={handleLogout}>
+                        <View style={styles.menuItem}>
+                            <Icon name='sign-out' color="#ef3737" size={25} />
+                            <Text style={styles.menuItemText}>Logout</Text>
+                        </View>
                     </TouchableOpacity>
                 </View>
             </View>
-            
+        }
         </View>
+        
 
     );
 }
 
 const styles = StyleSheet.create({
-    // container:{
-    //   backgroundColor:'#FBE8DA'
-    // },
+
     title: {
         padding: 10
     },
@@ -93,31 +110,47 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 35,
 
     },
-    imageView :{
-        alignItems : 'center',
-        marginTop : -50
+    imageView: {
+        alignItems: 'center',
+        marginTop: -50
     },
-    userName : {
-        fontSize : 23,
-        lineHeight : 35,
-        fontWeight : '500',
-        letterSpacing : 1
+    userName: {
+        fontSize: 23,
+        lineHeight: 35,
+        fontWeight: '500',
+        letterSpacing: 1
     },
-    greeting : {
+    greeting: {
         lineHeight: 74,
         letterSpacing: 2
     },
-    menuItem : {
-        flexDirection : 'row',
-        paddingVertical : 15,
-        paddingHorizontal : 30,
-        marginTop: 200
+    info: {
+        flexDirection: 'column',
+        marginLeft: 45,
+        
     },
-    menuItemText : {
-        color : '#ef3737',
-        marginLeft : 20,
-        fontWeight : '600',
-        fontSize : 16,
-        lineHeight : 26
+    majorsection :{
+        flex : 1,
+        flexDirection : 'row'
+        
+    },
+    majorText :{
+        color : '#24447C',
+        fontSize : 18,
+        fontWeight : '600'
+    },
+    menuItem: {
+        flexDirection: 'row',
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+        marginTop: 120,
+        marginLeft : 15
+    },
+    menuItemText: {
+        color: '#ef3737',
+        marginLeft: 20,
+        fontWeight: '600',
+        fontSize: 16,
+        lineHeight: 26
     }
 });
--- server/api.js
+++ server/api.js
@@ -6,29 +6,7 @@ import AsyncStorage from '@react-native-community/async-storage';
 // import  firebase from 'react-native-firebase' 
 import firebase from 'firebase';
 import config from './../firebase/firebaseconfig'
-// import firestore from './..//node_modules/@firebase/firestore'
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDNCpbTt3KidZ7Y_X_kFRlVxe9ycP4Iz9Q",
-//     authDomain: "medlick.firebaseapp.com",
-//     projectId: "medlick",
-//     storageBucket: "medlick.appspot.com",
-//     messagingSenderId: "584700422330",
-//     appId: "1:584700422330:web:13f4ae2de82975358bba83"
-// };
-// if (!firebase.apps.length) {
-//     firebase.initializeApp(config);
-//   } else {
-//       firebase.app(); // if already initialized
-//   }
-
-//   const db = firebase.firestore();
-//   db.settings({
-//    timestampsInSnapshots: true
-//   });  
-// if (!firebase.app.length) {
-//     firebase.initializeApp(firebaseConfig);
-//   }
 
 const BASE_URL = 'http://127.0.0.1:8000/api';
 // const BASE_URL = 'http://medical-clinic.tk/api';
@@ -59,9 +37,15 @@ export default {
         return res;
     },
 
+    // getUserInfo: async () => {
+    //     let header = await getHeader();
+    //     let res = await axios.get(`${BASE_URL}/userinfo`, header);
+    //     return res;
+    // },
+
     getUserInfo: async () => {
         let header = await getHeader();
-        let res = await axios.get(`${BASE_URL}/userinfo`, header);
+        let res = await axios.get(`${BASE_URL}/getLoggedDoctor`, header);
         return res;
     },
 
@@ -81,17 +65,7 @@ export default {
         return res;
     }
 
-    // getMessages : async()=>{
-    //     const messages = await db.collection('messages').doc('1-2').collection('1-2').onSnapshot(snapchot=>{
-    //         snapchot.docChanges().forEach(change=>{
-    //             console.log(change)
-    //         })
-    //     });
-    //     // const messages = await db
 
-
-    //     return messages;
-    // }
 
 
 
